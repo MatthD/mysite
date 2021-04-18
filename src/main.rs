af14755a1097e5c747faa3e774d91a75f4e0beec
MatthD <matthias.dieudonne@gmail.com>
2021-04-18 15:55:26 +0200

    fix(index): fix index

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use actix_files::NamedFile;
 use actix_web::{HttpRequest, Result};
 use std::path::PathBuf;
+use actix_files as fs;
 
 async fn index(_req: HttpRequest) -> Result<NamedFile> {
     let path: PathBuf = "./src/front/public/index.html".parse().unwrap();
@@ -11,7 +12,9 @@ async fn index(_req: HttpRequest) -> Result<NamedFile> {
 async fn main() -> std::io::Result<()> {
     use actix_web::{web, App, HttpServer};
 
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+    HttpServer::new(||{
+        App::new().service(fs::Files::new("/", "./src/front/public").show_files_listing().index_file("index.html"))
+    })
         .bind("0.0.0.0:8080")?
         .run()
         .await
